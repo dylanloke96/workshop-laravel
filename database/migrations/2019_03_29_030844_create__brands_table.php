70992ee787f63546bff529dc8d3c97c41ec6c129
unknown <dylanloke96@gmail.com>
2019-03-30 16:03:06 +0800

    first commit

---
+++ database/migrations/2019_03_29_030844_create__brands_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBrandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_brands', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Name');
+            $table->text('Description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_brands');
+    }
+}
